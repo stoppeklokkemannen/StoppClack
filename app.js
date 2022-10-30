1a9f4022560a5fcd181e172921749cda5e98fa46
stoppeklokkemannen <sondreostland@outlook.com>
2022-10-30 15:48:28 +0100

    This is my work guys dont steal it use it.

---
+++ app.js
@@ -0,0 +1,36 @@
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const stopBtn = document.querySelector('.stop-btn');
+const textArea = document.querySelector('h1');
+
+let counter = 0;
+let interval = null;
+
+const stopwatch = ()=> {
+    counter ++;
+    console.log(counter);
+    let hours = Math.floor(counter/3600);
+    let mins = Math.floor((counter - hours * 3600) /60);
+    let seconds = counter % 60;
+    
+    
+    textArea.textContent = `${hours} : ${mins} : ${seconds}`;
+};
+const startStopwatch = ()=>{
+    interval = setInterval(stopwatch, 1000);
+    };
+    const stopStopwatch =()=>{
+        clearInterval(interval)
+        interval = null;
+    };
+    const resetStopwatch = ()=> {
+        stopStopwatch();
+        counter = 0;
+        textArea.textContent = `00 : 00 : 00`;
+    };
+
+    startBtn.addEventListener('click',startStopwatch);
+    resetBtn.addEventListener('click',resetStopwatch);
+    stopBtn.addEventListener('click',stopStopwatch);
+
+
