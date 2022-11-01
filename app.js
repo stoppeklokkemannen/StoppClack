f4e0f1d19e45bff363529e2694fedcf31ce07710
Fillip <filliplangkas@gmail.com>
2022-11-01 20:09:23 +0100

    added fps

--- app.js
+++ app.js
@@ -33,4 +33,20 @@ const startStopwatch = ()=>{
     resetBtn.addEventListener('click',resetStopwatch);
     stopBtn.addEventListener('click',stopStopwatch);
 
+    var fps = document.getElementById("fps");
+    var startTime = Date.now();
+    var frame = 0;
+    
+    function tick() {
+      var time = Date.now();
+      frame++;
+      if (time - startTime > 1000) {
+          fps.innerHTML = (frame / ((time - startTime) / 1000)).toFixed(1);
+          startTime = time;
+          frame = 0;
+        }
+      window.requestAnimationFrame(tick);
+    }
+    tick();
+
 
