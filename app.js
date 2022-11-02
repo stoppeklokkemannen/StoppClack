f524aa09ca7fdfce508d82f547fad4ff540c1d6a
Kevin00812 <kevinbjorvand@gmail.com>
2022-11-02 21:32:32 +0100

    MILLISEKUNDER

--- app.js
+++ app.js
@@ -3,7 +3,54 @@ const resetBtn = document.querySelector('.reset-btn');
 const stopBtn = document.querySelector('.stop-btn');
 const textArea = document.querySelector('h1');
 
-let counter = 0;
+var timeBegan = null
+    , timeStopped = null
+    , stoppedDuration = 0
+    , started = null;
+
+function start() {
+    if (timeBegan === null) {
+        timeBegan = new Date();
+    }
+
+    if (timeStopped !== null) {
+        stoppedDuration += (new Date() - timeStopped);
+    }
+    console.log(stoppedDuration);
+
+    started = setInterval(clockRunning, 10);	
+}
+
+function stop() {
+    timeStopped = new Date();
+    clearInterval(started);
+}
+ 
+function reset() {
+    clearInterval(started);
+    stoppedDuration = 0;
+    timeBegan = null;
+    timeStopped = null;
+    document.getElementById("display-area").innerHTML = "00:00:00.000";
+}
+
+function clockRunning(){
+    var currentTime = new Date()
+        , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
+        , hour = timeElapsed.getUTCHours()
+        , min = timeElapsed.getUTCMinutes()
+        , sec = timeElapsed.getUTCSeconds()
+        , ms = timeElapsed.getUTCMilliseconds();
+
+    document.getElementById("display-area").innerHTML = 
+        (hour > 9 ? hour : "0" + hour) + ":" + 
+        (min > 9 ? min : "0" + min) + ":" + 
+        (sec > 9 ? sec : "0" + sec) + "." + 
+        (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+};
+
+
+/*let counter = 0;
 let interval = null;
 
 const stopwatch = ()=> {
@@ -34,7 +81,7 @@ const startStopwatch = ()=>{
 
     startBtn.addEventListener('click',startStopwatch);
     resetBtn.addEventListener('click',resetStopwatch);
-    stopBtn.addEventListener('click',stopStopwatch);
+    stopBtn.addEventListener('click',stopStopwatch); */
 
 /////
 
